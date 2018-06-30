5282349357fab23c74eabfa04bc24f7407526dee
Levan Beroshvili <lberoshvili9@gmail.com>
2018-06-30 06:13:24 +0400

    started writing visual of header

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express()
 app.use('/public', express.static('public'))
 app.use('/public', express.static('node_modules'))
 
-app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.get('/*', (req, res) => res.sendFile(__dirname + '/public/indexNew.html'))
 
 app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'))
